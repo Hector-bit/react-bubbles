5b50711a886f5e710f0ca93665d7e8217fca802c
Hector-bit <hectormartinez.2978@gmail.com>
2019-10-18 10:25:56 -0700

    colors show up now

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,20 +1,73 @@
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+
+// import Bubbles from "./Bubbles";
+// import ColorList from "./ColorList";
+// import { axiosWithAuth } from "../utils/axiosWithAuth";
+
+// const BubblePage = () => {
+//   const [colorList, setColorList] = useState([]);
+
+//   axios
+//   .post('http://localhost:5000/api/colors')
+//   .then(res => {
+//     console.log(res);
+//     colorList(res);
+//   })
+//   .catch(err => {
+//     console.log(err)
+//   })
+//   // fetch your colors data from the server when the component mounts
+//   // set that data to the colorList state property
+
+//   return (
+//     <>
+//       <ColorList colors={colorList} updateColors={setColorList} />
+//       <Bubbles colors={colorList} />
+//     </>
+//   );
+// };
+
+// export default BubblePage;
+
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
-const BubblePage = () => {
-  const [colorList, setColorList] = useState([]);
-  // fetch your colors data from the server when the component mounts
-  // set that data to the colorList state property
+class BubblePage extends React.Component {
+    state = {
+        colorList: []
+    };
 
-  return (
-    <>
-      <ColorList colors={colorList} updateColors={setColorList} />
-      <Bubbles colors={colorList} />
-    </>
-  );
-};
+    componentDidMount() {
+        this.getData();
+    }
+
+    getData = () => {
+        axiosWithAuth()
+            .get('/api/colors')
+            .then(res => {
+                // console.log(res.data)
+                const color = res.data
+                this.setState({ colorList: color })
+            })
+            .catch(res => {
+                console.log(res)
+            })
+    }
+
+  render(){
+    console.log(this.state.colorList)
+    return (
+      <>
+        <ColorList colors={this.state.colorList} updateColors={this.state.colorList} />
+        <Bubbles colors={this.state.colorList} />
+      </>
+    )
+}
+}
 
 export default BubblePage;
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,80 @@
-import React from "react";
+// import React from "react";
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
+// const Login = () => {
+//   // make a post request to retrieve a token from the api
+//   // when you have handled the token, navigate to the BubblePage route
+//   return (
+//     <>
+//       <h1>Welcome to the Bubble App!</h1>
+//       <p>Build a login page here</p>
+//     </>
+//   );
+// };
+
+// export default Login;
+
+import React from 'react';
+
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+import { Redirect } from 'react-router-dom';
+
+class Login extends React.Component {
+  state = {
+    credentials: {
+      username: '',
+      password: ''
+    }
+  };
+
+  handleChange = e => {
+    this.setState({
+      credentials: {
+        ...this.state.credentials,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  login = e => {
+    e.preventDefault();
+    // login to retrieve the JWT token
+    // add the token to localstorage
+    // route to /protected (whatever landing page)
+    axiosWithAuth()
+      .post('/api/login', this.state.credentials)
+      .then(res => {
+        console.log(res.data.payload)
+        localStorage.setItem('token', res.data.payload);
+        this.props.history.push('/bubblepage');
+      })
+      .catch(err => console.log(err.response));
+  };
+
+  render() {
+    // if (localStorage.getItem('token')) {
+    //   return <Redirect to="/login" />;
+    // }
+    return (
+      <div>
+        <form onSubmit={this.login}>
+          <input
+            type="text"
+            name="username"
+            value={this.state.credentials.username}
+            onChange={this.handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            value={this.state.credentials.password}
+            onChange={this.handleChange}
+          />
+          <button>Log in</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default Login;
+
